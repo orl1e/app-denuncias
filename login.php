e5b7dd35c1c0eb6ef26fbba66eccc3ec339d445e
01603 <orliemacias507@gmail.com>
2023-12-30 02:10:43 -0500

    Rediseño de página de login y creación de form para creación de usuarios

--- login.php
+++ login.php
@@ -21,32 +21,69 @@ if (isset($_SESSION['usuario']) && isset($_SESSION['codigoValido'])) {
 ?>
 <body>
 
-    <div class="container-sm m-5 form">
+    <div class="container" id="container">
         <?php
 
             if (isset($_GET['error'])) {
             $mensajeError = urldecode($_GET['error']);
             echo '<p style="color: red;">' . $_SESSION['mensajeError'] . '</p>';
             } ?>
-    <h1><b>Inicio de sesión</b></h1>
-    <small class="text-body-secondary">Inicia sesión para usar la aplicación.</small>
+        <div class="form-container sign-up">
+            <form action="createAccount.php" method="post">
+                <h1><b>Crear cuenta</b></h1>
+                <small class="text-body-secondary">Ingresa tus datos para crear tu cuenta.</small>
+                <input type="text" name="nombre" placeholder="Ingrese su nombre de usuario" autofocus required>
+                <input type="text" name="correo" placeholder="Ingrese su correo" required>
+                <input type="password" name="password" placeholder="Ingrese su contraseña" required>
+                <input type="submit" value="Crear cuenta" name="submit">
+            </form>
+        </div>
 
-    <div class="mt-4">
-        <form action="loginValidation.php" method="post">
-            <label for="nombre"><b>Nombre usuario</b></label>
-            <input type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre" autofocus>
-            <label for="nombre"><b>Apellido usuario</b></label>
-            <input type="text" name="apellido" id="apellido" placeholder="Ingrese su apellido">
-            <label for="nombre"><b>Contraseña</label>
-            <input type="password" name="password" id="password" placeholder="Ingrese su contraseña">
-            <input type="submit" value="Iniciar sesión" name="submit">
-        </form>
+        <div class="form-container sign-in">
+            <form action="loginValidation.php" method="post">
+                <h1><b>Inicio de sesión</b></h1>
+                <small class="text-body-secondary">Inicia sesión para usar la aplicación.</small>
+                <input type="text" name="nombre" placeholder="Ingrese su nombre de usuario" autofocus>
+                <input type="password" name="password" id="password" placeholder="Ingrese su contraseña">
+                <a href="#">Olvidaste tu contraseña?</a>
+                <input type="submit" value="Iniciar sesión" name="submit">
+            </form>
+        </div>
+
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Bienvenido!</h1>
+                    <p>Introduce tus credenciales para usar el sitio 🚀</p>
+                    <button class="hidden" id="login">Iniciar sesión</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Nuevo por aquí?</h1>
+                    <p>Registrate para disfrutar el sitio 😁</p>
+                    <button class="hidden" id="register">Crear cuenta</button>
+                </div>
+            </div>
+        </div>
+
     </div>
 
-
-    </div>
-
-    <footer>Copyright © - Orlie Macías</footer>
+    <footer>Derechos Reservados © Orlie Macías</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script>
+        const container = document.getElementById('container');
+        const registerBtn = document.getElementById('register');
+        const loginBtn = document.getElementById('login');
+
+        document.addEventListener('DOMContentLoaded', function () {
+            registerBtn.addEventListener('click', () => {
+            container.classList.add("active");
+            });
+
+            loginBtn.addEventListener('click', () => {
+                container.classList.remove("active");
+            });
+        });
+        
+    </script>
 </body>
 </html>
